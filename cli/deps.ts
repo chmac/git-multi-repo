a9bfb7af2e388c0c49157be88f5526018d4c5404
Callum Macdonald <git@callum-macdonald.com>
2023-02-20 22:29:50 +0100

    Add path.

--- cli/deps.ts
+++ cli/deps.ts
@@ -1,4 +1,5 @@
 export * as colours from "https://deno.land/std@0.177.0/fmt/colors.ts";
+export * as path from "https://deno.land/std@0.177.0/path/mod.ts";
 export * as cliffy from "https://deno.land/x/cliffy@v0.25.7/mod.ts";
 export { run } from "https://deno.land/x/run_simple@1.1.0/mod.ts";
 import { default as debug_module } from "https://deno.land/x/debuglog@v1.0.0/debug.ts";
