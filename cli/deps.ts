3db7aec7c474cede29a4c03d33358946f3173b6e
Callum Macdonald <git@callum-macdonald.com>
2023-02-20 22:15:39 +0100

    More deps.

--- cli/deps.ts
+++ cli/deps.ts
@@ -1,1 +1,6 @@
+export * as colours from "https://deno.land/std@0.177.0/fmt/colors.ts";
+export * as cliffy from "https://deno.land/x/cliffy@v0.25.7/mod.ts";
 export { run } from "https://deno.land/x/run_simple@1.1.0/mod.ts";
+import { default as debug_module } from "https://deno.land/x/debuglog@v1.0.0/debug.ts";
+
+export const debug_root = debug_module("git-multi-repo");
