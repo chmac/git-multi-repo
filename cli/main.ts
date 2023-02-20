463dd136de00863c1b4b401d7e111bc095bb81cd
Callum Macdonald <git@callum-macdonald.com>
2023-02-20 22:30:03 +0100

    Read config file from home dir.

--- cli/main.ts
+++ cli/main.ts
@@ -1,30 +1,42 @@
-import { cliffy, colours, debug_root, run } from "./deps.ts";
+import { cliffy, colours, debug_root, path, run } from "./deps.ts";
 
 type Repo = {
   name: string;
   path: string;
 };
+type Config = {
+  repos: Repo[];
+};
 
-const REPOS: Repo[] = [];
+const CONFIG_FILE_NAME = ".git-multi-repo.json";
+
+const debug = debug_root.extend("main");
+
+async function loadConfig(home: string) {
+  const configPath = path.join(home, CONFIG_FILE_NAME);
+  const json = await Deno.readTextFile(configPath);
+  const config = JSON.parse(json) as Config;
+  return config;
+}
 
 async function getGitStatus(path: string, verbose = false) {
   const statusLines = await run("git status --short", { cwd: path, verbose });
   return statusLines;
 }
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-}
-
-const debug = debug_root.extend("main");
-
 await new cliffy.Command()
   .name("git-multi-repo")
   .version("0.1.0")
+  .globalEnv("HOME=<path:string>", "The user's home directory", {
+    required: true,
+  })
   .command("status")
-  .action(async () => {
+  .action(async (options) => {
+    const config = await loadConfig(options.home);
+
+    const { repos } = config;
     const repoStatuses = await Promise.allSettled(
-      REPOS.map(async (repo) => {
+      repos.map(async (repo) => {
         const statusLines = await getGitStatus(repo.path);
         const hasChanges = statusLines.length !== 0;
         return { ...repo, statusLines, hasChanges };
